5789bcf9d5a6b451e015f7c9ec88fdd4d497f330
Manoj Borugadda <borugadda.manoj@gmail.com>
2026-01-09 09:42:58 +0530

    view_patients endpoint

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
-
+import json
 app = FastAPI()
+
+# Load patient data from a JSON file
+def load_data():
+    with open("patients.json","r") as f:
+        data = json.load(f)
+    return data
+
 
 @app.get("/")
 def hello_world():
-    return {"message":"hello world, we are learning fastapi !"}
+    return {"message":"Patient Management System API !"}
 
 @app.get("/about")
 def about():
-    return {"message":"This is a simple FastAPI application."}
+    return {"message":"A fully functional API to manage your patient records."}
+
+@app.get("/view_patients")
+def view_patients():
+    data = load_data()
+    return data
