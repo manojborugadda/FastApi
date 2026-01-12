cd645e55fed0a4c8af4e815dbac32dccb7b91ca4
Manoj Borugadda <borugadda.manoj@gmail.com>
2026-01-12 08:40:48 +0530

    messgae not found error using PATH()

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path
 import json
 app = FastAPI()
 
@@ -21,3 +21,10 @@ def about():
 def view_patients():
     data = load_data()
     return data
+
+@app.get("/patient/{patient_id}")
+def view_patient(patient_id:str = Path(...,description="id of the patient in the Database",example="P001")):
+    data = load_data()
+    if patient_id in data:
+        return data[patient_id]
+    return { "message":"not found in the database"}
